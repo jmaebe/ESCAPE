9420f49b60c725e249d77bc69bbcd0830247dc9c
jmaebe <jmaebe@1d94b73f-039a-4ceb-9c95-bfa3836eab3a>
2010-03-29 21:22:13 +0000

    * convert TFPColor of colors[] property into a TColor used by the brush     in Lazarus (in Delphi, both use TColor)
    git-svn-id: https://acavus.elis.ugent.be/svn/escape/trunk@14 1d94b73f-039a-4ceb-9c95-bfa3836eab3a

--- activity.pas
+++ activity.pas
@@ -175,7 +175,7 @@ begin
     begin
       if Length(s)>0 then
       begin
-        Brush.Color:=Colors[Col-1,Row-1];
+        Brush.Color:=FPColorToTColor(Colors[Col-1,Row-1]);
         FillRect(Rect);
         if Stalled[Col-1,Row-1] then
         begin
